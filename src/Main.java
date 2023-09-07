c572d880b970e98829f6f1ec46ffb2503df880da
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-07 15:40:56 +0600

    Loops refresher

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int number = 23;
+
         while(number<=35){
-            System.out.println("Okay " + number);
+            System.out.print(number + " ");
             number+=2;
+        }
+        System.out.println(" ");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.print(i + " ");
         }
     }
 }
