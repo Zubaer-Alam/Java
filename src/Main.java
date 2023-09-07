05d9713786e29a55c6a4f3e3427ac56978be90ed
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-07 14:52:01 +0600

    While Loop

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //8 primitive dataTypes:
-
-        boolean aBool = true; // 1 bit
-
-        byte aByte = 100; // 8 bits
-        short aShort = 93; // 16 bits
-        int aValue = 10; // 32 bits
-        long aLong = 8304;// 64 bits
-
-        float aFloat = 5.233f; // 32 bits
-        double aDouble = 3.4553; // 64 bits
-
-        char aChar = 'b'; // 16 bits
-
-        String text = "Hi";
-        String blank = " ";
-        String moreText = "There";
-        String total = text + blank + moreText;
-        System.out.println(text + blank + moreText);
-        System.out.println(total);
-        System.out.println(total.length());
+        int number = 23;
+        while(number<=35){
+            System.out.println("Okay " + number);
+            number+=2;
+        }
     }
 }
---
+++ src/dataTypes.java
@@ -0,0 +1,25 @@
+public class dataTypes {
+    public static void dataTypes() {
+        //8 primitive dataTypes:
+
+        boolean aBool = true; // 1 bit
+
+        byte aByte = 100; // 8 bits
+        short aShort = 93; // 16 bits
+        int aValue = 10; // 32 bits
+        long aLong = 8304;// 64 bits
+
+        float aFloat = 5.233f; // 32 bits
+        double aDouble = 3.4553; // 64 bits
+
+        char aChar = 'b'; // 16 bits
+
+        String text = "Hi";
+        String blank = " ";
+        String moreText = "There";
+        String total = text + blank + moreText;
+        System.out.println(text + blank + moreText);
+        System.out.println(total);
+        System.out.println(total.length());
+    }
+}
