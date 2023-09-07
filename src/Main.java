92320699e9ec6f737801a0806dbf350d3d5bd8eb
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-07 14:24:08 +0600

    Primitive dataTypes refresher

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        //8 primitive dataTypes:
+
+        boolean aBool = true; // 1 bit
+
+        byte aByte = 100; // 8 bits
+        short aShort = 93; // 16 bits
+        int aValue = 10; // 32 bits
+        long aLong = 8304;// 64 bits
+
+        float aFloat = 5.233f; // 32 bits
+        double aDouble = 3.4553; // 64 bits
+
+        char aChar = 'b'; // 16 bits
+        System.out.println(aBool);
+        System.out.println(aByte);
+        System.out.println(aShort);
+        System.out.println(aValue);
+        System.out.println(aLong);
+        System.out.println(aFloat);
+        System.out.println(aDouble);
+        System.out.println(aChar);
     }
 }
