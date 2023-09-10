cba403b77953b0db8f0bb13dc65f529cc967c039
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-10 22:13:32 +0600

    Static and Final

--- src/Application.java
+++ src/Application.java
@@ -1,19 +1,24 @@
 class Student {
+    public static final int MY_CONSTANT = 8;
     public String name;
 
     // Static variables also known as class variables, or
     // instance variables don't send copies to every object created.
     public static String studentClass;
+    public static int count = 0;
+    public Student(){
+        count++;
+    }
 
-    public void studentName(){
+    public void studentInfo(){
         System.out.println(name+" is enrolled in "+studentClass);
         // Non-static methods can access static variables
     }
 
-    // Static methods can access static variables only.
+
     public static void classInfo(){
         System.out.println(studentClass);
-
+        //Static methods can access static variables only.
         //System.out.println(name);
         //will not work.
     }
@@ -23,9 +28,12 @@ public class Application{
         Student.studentClass = "Web development";
         Student.classInfo();
         System.out.println(Student.studentClass);
+        System.out.println(Student.MY_CONSTANT);
 
+        System.out.println("Before creating object count = "+Student.count);
         Student student1 = new Student();
         Student student2 = new Student();
+        System.out.println("After creating object, count = "+Student.count);
 
         student1.name = "Zubaer";
         student2.name = "Sunny";
@@ -33,7 +41,7 @@ public class Application{
         System.out.println(student1.name);
         System.out.println(student2.name);
 
-        student1.studentName();
-        student2.studentName();
+        student1.studentInfo();
+        student2.studentInfo();
     }
 }
