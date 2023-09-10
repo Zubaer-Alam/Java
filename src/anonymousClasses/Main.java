a8e4b43f570d0aa577c8c8f991476265b24e7a8a
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-11 02:15:34 +0600

    Anonymous classes

---
+++ src/anonymousClasses/Main.java
@@ -0,0 +1,32 @@
+package anonymousClasses;
+
+class Computer{
+    public void shutDown(){
+        System.out.println("SHUTTING DOWN");
+    }
+}
+
+interface Phone{
+    public void call();
+}
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        Computer computer1 = new Computer(){
+            @Override
+            public void shutDown(){
+                System.out.println("MACBOOK SHUTTING DOWN");
+            }
+        };
+        computer.shutDown();
+        computer1.shutDown();
+
+        Phone phone = new Phone() {
+            @Override
+            public void call() {
+                System.out.println("CALLING...");
+            }
+        };
+        phone.call();
+    }
+}
