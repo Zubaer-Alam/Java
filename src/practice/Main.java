18a31cde94b4bc52657e85ba9e544260347ae88b
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-10 22:50:38 +0600

    Access Modifiers

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,6 +1,12 @@
 package practice;
 
 public class Main {
+    /*Access modifiers:
+       Public: Can access these methods from any class across packages.
+       Protected: Can access these methods from any class in same package.
+       Default: Similar to Public, given in absence of access modifier.
+       Private: Can access these methods within class.
+     */
     public static void main(String[] args) {
         Factory factory1 = new Factory();
 
