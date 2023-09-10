06d4a84a7c3cdf84be826620b5f2b97429054fcb
Zubaer-Alam <zobaeralam@iut-dhaka.edu>
2023-09-10 19:53:22 +0600

    Constructors detailed

---
+++ src/Constructors.java
@@ -0,0 +1,24 @@
+class Program{
+    int value;
+    String text;
+    public Program(){
+        this(2);
+        System.out.println("First Constructor");
+    }
+    public Program(int value){
+        this.value = value;
+        System.out.println("Second Constructor");
+    }
+    public Program(int value,String text){
+        this.value = value;
+        this.text = text;
+        System.out.println("Third Constructor");
+    }
+}
+public class Constructors {
+    public static void main(String[] args) {
+        Program myProgram0 = new Program();
+        Program myProgram1 = new Program(2);
+        Program myProgram2 = new Program(5,"Hello");
+    }
+}
--- src/Loops.java
+++ src/Loops.java
@@ -1,9 +1,7 @@
-import org.w3c.dom.ls.LSInput;
-
 import java.util.Scanner;
 
 public class Loops {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         int limit = input.nextInt();
