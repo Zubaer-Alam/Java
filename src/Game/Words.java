d80f1761b957886308e1759ac8c4bfe54505fe14
Zubaer-Alam <xuvaer@gmail.com>
2023-09-14 04:11:09 +0600

    🕹️ Setting up the Game

---
+++ src/Game/Words.java
@@ -0,0 +1,16 @@
+package Game;
+
+public class Words {
+    private String[] randomWords = {"animals", "happiness", "indefinite", "steady",
+            "birthday", "extreme", "rights", "properties", "ceremony", "independence",
+            "beneath", "information","students" ,"employee"};
+
+    private String selectedWord;
+
+    public Words(){
+        selectedWord = randomWords[3];
+    }
+    public String toString(){
+        return selectedWord;
+    }
+}
---
+++ src/Game/guessTheWord.java
@@ -0,0 +1,23 @@
+package Game;
+
+public class guessTheWord {
+    private boolean play = false;
+    private Words randomWord = new Words();
+    public void start(){
+
+        do {
+            showWord();
+            getInput();
+            checkInput();
+        }while (play);
+    }
+    void showWord(){
+        System.out.println(randomWord);
+    }
+    void getInput(){
+        System.out.println("getInput");
+    }
+    void checkInput(){
+        System.out.println("checkInput");
+    }
+}
