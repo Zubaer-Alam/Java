46cff8552a730737a2435cdc4d54f8abb84dafa7
Zubaer-Alam <xuvaer@gmail.com>
2023-09-16 04:03:45 +0600

    ✔️ Serialization and deserialization

--- src/Serialization/Reading.java
+++ src/Serialization/Reading.java
@@ -3,22 +3,30 @@ package Serialization;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class Reading {
     public static void main(String[] args) {
         System.out.println(("Reading objects..."));
 
-        try(FileInputStream fileInput = new FileInputStream("Vehicle.data")){
-
+        try (FileInputStream fileInput = new FileInputStream("Vehicle.data")) {
             ObjectInputStream objectInput = new ObjectInputStream(fileInput);
 
-            Vehicle vehicle1 = (Vehicle)objectInput.readObject();
-            Vehicle vehicle2= (Vehicle)objectInput.readObject();
+            Vehicle[] vehicle = (Vehicle[])objectInput.readObject();
+
+            ArrayList<Vehicle> vehicleList = (ArrayList<Vehicle>) objectInput.readObject();
+
+            for(Vehicle v : vehicle){
+                System.out.println(v);
+            }
+
+            System.out.println();
+
+            for(Vehicle v:vehicleList){
+                System.out.println(v);
+            }
 
             objectInput.close();
-
-            System.out.println(vehicle1);
-            System.out.println(vehicle2);
 
         } catch (ClassNotFoundException | IOException e) {
             throw new RuntimeException(e);
--- src/Serialization/Writing.java
+++ src/Serialization/Writing.java
@@ -1,28 +1,28 @@
 package Serialization;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+     serialization refers to the process of converting Java objects into a format that can be easily stored in a file. This allows us to persistently save these objects to a file, and later, if needed, deserialize (convert them back) to their original Java object form.
+ */
 
 public class Writing {
     public static void main(String[] args) {
         System.out.println("Writing Objects...");
 
-        Vehicle car = new Vehicle(1, "Toyota");
-        Vehicle truck = new Vehicle(2, "Ford");
+        Vehicle[] vehicles = {new Vehicle(1, "Toyota"),
+                new Vehicle(2, "Honda"),
+                new Vehicle(3, "Ford")};
 
-        System.out.println(car);
-        System.out.println(truck);
+        ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>(Arrays.asList(vehicles));
 
-        try (FileOutputStream fileOutput = new FileOutputStream("Vehicle.data")) {
+        try (FileOutputStream fileOutput = new FileOutputStream("Vehicle.data");
+             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput)) {
 
-            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
-
-            objectOutput.writeObject(car);
-            objectOutput.writeObject(truck);
-
-            objectOutput.close();
+            objectOutput.writeObject(vehicles);
+            objectOutput.writeObject(vehicleList);
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
